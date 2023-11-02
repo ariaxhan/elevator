33e4e573138c857c0ef8920c96a746399a5ae8ba
Aria <113392746+ariaxhan@users.noreply.github.com>
2023-11-01 23:27:28 -0700

    figuring out

---
+++ main.java
@@ -0,0 +1,106 @@
+import java.io.FileReader;
+import java.util.Properties;
+import java.util.Random;
+import java.util.*;
+import java.io.*;
+
+public class main {
+
+  // function to read in properties file
+  public static variables readFile(String fileName, variables v) {
+    try {
+      // create reader object
+      FileReader reader = new FileReader("test.properties");
+      // create properties objectd
+      Properties p = new Properties();
+      // add a wrapper around reader object
+      p.load(reader);
+      // access data and fill in variables object
+      v.setStructures(p.getProperty("structures"));
+      v.setFloors(Integer.parseInt(p.getProperty("floors")));
+      v.setPassengers(Double.parseDouble(p.getProperty("passengers")));
+      v.setElevators(Integer.parseInt(p.getProperty("elevators")));
+      v.setElevatorCapacity(
+        Integer.parseInt(p.getProperty("elevatorCapacity"))
+      );
+      v.setDuration(Integer.parseInt(p.getProperty("duration")));
+
+      // close reader
+      reader.close();
+    } catch (Exception e) {
+      // if the file is not found, fill in with default values
+      v.setDefault();
+    }
+    // test print
+    v.print();
+    return v;
+  }
+
+  public static void runSimulation(variables v) {
+    // function to run simulation
+    // during each "tick" in the duration variable, one of the following may occur
+    for (int i = 0; i < v.getDuration(); i++) {
+      // randomly generate a number between 1 and 3 to determine which of the following
+      Random rand = new Random();
+      int n = rand.nextInt(3) + 1;
+      // Elevator unload & load: An elevator may stop at a floor and unload all passengers in the elevator
+      // bound for that floor. Additionally, during the same “tick”, any passengers on the floor waiting for
+      // an elevator going in the desired direction (up or down). You may assume that passengers never
+      // enter an elevator going in the wrong direction.
+
+      // Elevator travel: An elevator may travel between no more than 5 floors (eg. from the 8th floor to
+      // the 13th floor).
+      //every multiple passengers
+      // use queue, up and down
+
+      // New passengers: Given “passengers” — the probabilities in the property file — a new passenger
+      // may appear on a floor and request transportation to another floor
+    }
+  }
+
+  // linkedlist, arraylist
+  // linked queue
+
+  // use an array to store a heap
+  // down max heap
+  // up min heap
+
+  // list interface to implement linkedlist and arraylist
+  // queue interface to implement linkedqueue and arrayqueue
+
+  private class Elevator {
+      private int currentFloor = 0;
+      private int direction = 1; // 1 for up, -1 for down
+      private int capacity = 0;
+      private List<Passenger> passengers;
+  }
+  
+  private class Passenger {
+      int startFloor;
+      int endFloor;
+      int startTime;
+
+      // constructor
+      public Passenger(int startFloor, int endFloor, int startTime) {
+          this.startFloor = startFloor;
+          this.endFloor = endFloor;
+          this.startTime = startTime;
+      }
+
+      // determining direction
+        public int getDirection() {
+            if (startFloor < endFloor) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+  }
+
+  public static void main(String[] args) {
+    // create variables object
+    variables v = new variables();
+    // read in properties file
+    readFile("test.properties", v);
+  }
+}
--- variables.java
+++ variables.java
@@ -9,7 +9,16 @@ public class variables {
   int duration;
 
   // constructor
-  public variables(){};
+  public variables() {}
+
+  public void setDefault() {
+    this.structures = "linked";
+    this.floors = 32;
+    this.passengers = 0.03;
+    this.elevators = 1;
+    this.elevatorCapacity = 10;
+    this.duration = 500;
+  }
 
   // getters
   public String getStructures() {
@@ -33,67 +42,67 @@ public class variables {
   }
 
   public int getDuration() {
-      return duration;
+    return duration;
   }
-  
+
   // setters including validation, invalid values will be replaced with default values
   public void setStructures(String structures) {
-      if (structures.equals("linked")) {
-          this.structures = structures;
-      } else if (structures.equals("array")) {
-          this.structures = structures;
-      } else {
-          this.structures = "linked";
-      }
-  }
-    
-  public void setFloors(int floors) {
-      if (floors >= 2) {
-          this.floors = floors;
-      } else {
-          this.floors = 32;
-      }
-  }
-    
-  public void setPassengers(double passengers) {
-      if (passengers >= 0 && passengers <= 1.0) {
-          this.passengers = passengers;
-      } else {
-          this.passengers = 0.03;
-      }
-  }
-    
-  public void setElevators(int elevators) {
-      if (elevators >= 1) {
-          this.elevators = elevators;
-      } else {
-          this.elevators = 1;
-      }
-  }
-    
-  public void setElevatorCapacity(int elevatorCapacity) {
-      if (elevatorCapacity >= 1) {
-          this.elevatorCapacity = elevatorCapacity;
-      } else {
-          this.elevatorCapacity = 10;
-      }
-  }
-    
-  public void setDuration(int duration) {
-      if (duration >= 1) {
-          this.duration = duration;
-      } else {
-          this.duration = 500;
-      }
-  }
-    
-  // print function to check input
-    public void print() {
-        System.out.println("structures: " + structures + "\n");
-        System.out.println("floors: " + floors + "\n");
-        System.out.println("passengers: " + passengers + "\n");
-        System.out.println("elevators: " + elevators + "\n");
-        System.out.println("elevatorCapacity: " + elevatorCapacity + "\n");
-        System.out.println("duration: " + duration + "\n");
+    if (structures.equals("linked")) {
+      this.structures = structures;
+    } else if (structures.equals("array")) {
+      this.structures = structures;
+    } else {
+      this.structures = "linked";
     }
+  }
+
+  public void setFloors(int floors) {
+    if (floors >= 2) {
+      this.floors = floors;
+    } else {
+      this.floors = 32;
+    }
+  }
+
+  public void setPassengers(double passengers) {
+    if (passengers >= 0 && passengers <= 1.0) {
+      this.passengers = passengers;
+    } else {
+      this.passengers = 0.03;
+    }
+  }
+
+  public void setElevators(int elevators) {
+    if (elevators >= 1) {
+      this.elevators = elevators;
+    } else {
+      this.elevators = 1;
+    }
+  }
+
+  public void setElevatorCapacity(int elevatorCapacity) {
+    if (elevatorCapacity >= 1) {
+      this.elevatorCapacity = elevatorCapacity;
+    } else {
+      this.elevatorCapacity = 10;
+    }
+  }
+
+  public void setDuration(int duration) {
+    if (duration >= 1) {
+      this.duration = duration;
+    } else {
+      this.duration = 500;
+    }
+  }
+
+  // print function to check input
+  public void print() {
+    System.out.println("structures: " + structures + "\n");
+    System.out.println("floors: " + floors + "\n");
+    System.out.println("passengers: " + passengers + "\n");
+    System.out.println("elevators: " + elevators + "\n");
+    System.out.println("elevatorCapacity: " + elevatorCapacity + "\n");
+    System.out.println("duration: " + duration + "\n");
+  }
 }
