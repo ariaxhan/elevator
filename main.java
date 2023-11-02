192b459927975ebcbbd5367a62f39ba3d00a22b4
Aria <113392746+ariaxhan@users.noreply.github.com>
2023-11-02 00:08:53 -0700

    confused

--- main.java
+++ main.java
@@ -7,38 +7,39 @@ import java.util.Random;
 
 public class main {
 
-    // function to read in properties file
-    public static variables readFile(String fileName, variables v) {
-        try {
-            // create reader object
-            FileReader reader = new FileReader("test.properties");
-            // create properties objectd
-            Properties p = new Properties();
-            // add a wrapper around reader object
-            p.load(reader);
-            // access data and fill in variables object
-            v.setStructures(p.getProperty("structures"));
-            v.setFloors(Integer.parseInt(p.getProperty("floors")));
-            v.setPassengers(Double.parseDouble(p.getProperty("passengers")));
-            v.setElevators(Integer.parseInt(p.getProperty("elevators")));
-            v.setElevatorCapacity(
-                    Integer.parseInt(p.getProperty("elevatorCapacity")));
-            v.setDuration(Integer.parseInt(p.getProperty("duration")));
+  // function to read in properties file
+  public static variables readFile(String fileName, variables v) {
+    try {
+      // create reader object
+      FileReader reader = new FileReader("test.properties");
+      // create properties objectd
+      Properties p = new Properties();
+      // add a wrapper around reader object
+      p.load(reader);
+      // access data and fill in variables object
+      v.setStructures(p.getProperty("structures"));
+      v.setFloors(Integer.parseInt(p.getProperty("floors")));
+      v.setPassengers(Double.parseDouble(p.getProperty("passengers")));
+      v.setElevators(Integer.parseInt(p.getProperty("elevators")));
+      v.setElevatorCapacity(
+        Integer.parseInt(p.getProperty("elevatorCapacity"))
+      );
+      v.setDuration(Integer.parseInt(p.getProperty("duration")));
 
-            // close reader
-            reader.close();
-        } catch (Exception e) {
-            // if the file is not found, fill in with default values
-            v.setDefault();
-        }
-        // test print
-        v.print();
-        return v;
+      // close reader
+      reader.close();
+    } catch (Exception e) {
+      // if the file is not found, fill in with default values
+      v.setDefault();
     }
+    // test print
+    v.print();
+    return v;
+  }
 
   public static void runSimulation(variables v) {
-      Random rand = new Random();
-      int n;
+    Random rand = new Random();
+    int n;
     // function to run simulation
     // during each "tick" in the duration variable, one of the following may occur
     for (int i = 0; i < v.getDuration(); i++) {
@@ -58,90 +59,120 @@ public class main {
     }
   }
 
-    // linkedlist, arraylist
-    // linked queue
+  // linkedlist, arraylist
+  // linked queue
 
-    // use an array to store a heap
-    // down max heap
-    // up min heap
+  // use an array to store a heap
+  // down max heap
+  // up min heap
 
-    // list interface to implement linkedlist and arraylist
-    // queue interface to implement linkedqueue and arrayqueue
+  // list interface to implement linkedlist and arraylist
+  // queue interface to implement linkedqueue and arrayqueue
 
-    private class Elevator {
-        private int currentFloor = 0;
-        private int direction = 1; // 1 for up, -1 for down
-        private int capacity = 0;
-        private List<Passenger> passengers;
-        private String structureType;
+  private class Elevator {
 
-        // constructor
-        public Elevator(int capacity, String structureType) {
-            this.capacity = capacity;
-            this.structureType = structureType;
-            if (structureType.equals("linked")) {
-                passengers = new LinkedList<Passenger>();
-            } else if (structureType.equals("array")) {
-                passengers = new ArrayList<Passenger>();
-            }
-        }
+    private int currentFloor = 0;
+    private int direction = 1; // 1 for up, -1 for down
+    private int capacity = 0;
+    private List<Passenger> passengers;
+    private String structureType;
+
+    // constructor
+    public Elevator(int capacity, String structureType) {
+      this.capacity = capacity;
+      this.structureType = structureType;
+      if (structureType.equals("linked")) {
+        passengers = new LinkedList<Passenger>();
+      } else if (structureType.equals("array")) {
+        passengers = new ArrayList<Passenger>();
+      }
     }
 
-    private class Passenger {
-        int startFloor;
-        int endFloor;
-        int startTime;
-        int direction;
-
-        // constructor
-        public Passenger(int startFloor, int endFloor, int startTime, int direction) {
-            this.startFloor = startFloor;
-            this.endFloor = endFloor;
-            this.startTime = startTime;
-            this.direction = direction;
-        }
-
-       // function to add passenger to a floor
-        public void addPassenger(Passenger p, List<Passenger> floor) {
-            floor.add(p);
-        }
-
+    // function to add passenger to elevator
+    public void addPassenger(Passenger p) {
+      passengers.add(p);
     }
 
-    private class generatePassenger {
-        private int floors;
-        private double passengersProbability;
-        private int direction; // 1 for up, -1 for down
-        // constructor
-
-        public generatePassenger(int floors, double passengersProbability) {
-            this.floors = floors;
-            this.passengersProbability = passengersProbability;
-        }
-
-        // function to generate a passenger
-        public Passenger generate() {
-            Random rand = new Random();
-            int startFloor = rand.nextInt(floors) + 1;
-            int endFloor = rand.nextInt(floors) + 1;
-            // make sure startFloor and endFloor are not the same
-            while (startFloor == endFloor) {
-                endFloor = rand.nextInt(floors) + 1;
-            }
-            // get direction
-            direction = getDirection(startFloor, endFloor);
-            return new Passenger(startFloor, endFloor, 0, direction);
-        }
-
-        // determining direction
-        public int getDirection(int startFloor, int endFloor) {
-            if (startFloor < endFloor) {
-                return 1;
-            } else {
-                return -1;
-            }
-        }
+    // function to remove passenger from elevator
+    public void removePassenger(Passenger p) {
+      passengers.remove(p);
     }
+
+    // function to Elevator unload & load: An elevator may stop at a floor and unload all passengers in the elevator
+    // bound for that floor. Additionally, during the same “tick”, any passengers on the floor waiting for
+    // an elevator going in the desired direction (up or down). You may assume that passengers never
+    // enter an elevator going in the wrong direction.
+    public void load() {
+      // check if elevator is full
+      if (passengers.size() == capacity) {
+        return;
+      } else {
+        // otherwise, check direction
+        if (direction == 1) {
+          // check if there are passengers on the floor going up
+
+        }
+      }
+    }
+    // Elevator travel: An elevator may travel between no more than 5 floors (eg. from the 8th floor to
+    // the 13th floor).
+    //every multiple passengers
+  }
+
+  private class Passenger {
+
+    int startFloor;
+    int endFloor;
+    int direction;
+
+    public Passenger(int startFloor, int endFloor) {
+      this.startFloor = startFloor;
+      this.endFloor = endFloor;
+    }
+
+    // function to add passenger to a floor
+    public void addPassenger(Passenger p, List<Passenger> floor) {
+      floor.add(p);
+    }
+
+    // function to remove passenger from a floor
+    public void removePassenger(Passenger p, List<Passenger> floor) {
+      floor.remove(p);
+    }
+
+    // determining direction
+    public int getDirection(int startFloor, int endFloor) {
+      if (startFloor < endFloor) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  }
+
+  private class generatePassenger {
+
+    private int floors;
+    private double passengersProbability;
+
+    // constructor
+    public generatePassenger(int floors, double passengersProbability) {
+      this.floors = floors;
+      this.passengersProbability = passengersProbability;
+    }
+
+    // function to generate a passenger
+    public Passenger generate() {
+      Random rand = new Random();
+      int startFloor = rand.nextInt(floors) + 1;
+      int endFloor = rand.nextInt(floors) + 1;
+      // make sure startFloor and endFloor are not the same
+      while (startFloor == endFloor) {
+        endFloor = rand.nextInt(floors) + 1;
+      }
+      return new Passenger(startFloor, endFloor, 0);
+    }
+  }
 
   public static void main(String[] args) {
     // create variables object
@@ -150,4 +181,3 @@ public class main {
     readFile("test.properties", v);
   }
 }
-
